5f0724697457929d6be8fcd2104c9e5fd2ede0dd
Daniel-gilabert <danielgilabert@prode.es>
2026-02-02 16:56:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -404,7 +404,7 @@ if st.button("➕ Añadir ausencia"):
       
 
 
-        st.success(f"{motivo_sel} añadida para {empleado_sel} del {desde} al {hasta}")
+       
 
 umbral_alerta = st.sidebar.slider("Umbral días sin fichar (grave)", 1, 10, 3)
 aplicar_todos_festivos = st.checkbox("Aplicar los festivos manuales a todos los empleados", value=True)
@@ -859,6 +859,7 @@ if st.button("⚙️ Procesar datos y generar informes"):
     )
 
 st.write("Fin de la app")
+
 
 
 
